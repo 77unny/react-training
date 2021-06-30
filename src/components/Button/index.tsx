e6bce8b60768b18b5e0b57389e73ad25ae351c5b
baekCode <baekcode@gmail.com>
2021-06-30 22:27:27 +0900

    [#0](feat) 컴포넌트별 tailwind 분리 (postcss)

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,22 +1,21 @@
 import React, { FC } from "react";
 import styled from "styled-components";
-import tw from "tailwind-styled-components/dist/tailwind";
+import tw from "tailwind-styled-components";
 
 const ContainerStyled = styled.button`
   &:hover {
     font-weight: bold;
   }
 `;
-const Container = tw(ContainerStyled)`
-  w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200
-`;
+const Container = tw(ContainerStyled)`custom-button`;
 
 interface IButtonProps {
   label: string;
+  onClick?: () => void;
 }
 
-const Button: FC<IButtonProps> = ({ label }) => {
-  return <Container>{label}</Container>;
+const Button: FC<IButtonProps> = ({ label, onClick }) => {
+  return <Container onClick={onClick}>{label}</Container>;
 };
 
 export default Button;
